0047f08faf4618be1ad09c2cf947c93d162e0d90
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2025-02-13 18:06:29 +0100

    feat(front): create react architecture and install package
    install axios,redux,redux toolkit,router

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { useContext, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <Router> 
+      <Routes>
+        
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
